33c8b78b1d8070f640b4dacfc19dbe79c544883a
ᯓ‌𐌌།⃪᱂ ⚠️𝑻𝒉𝒂𝒌𝒖𝒓𓂃 <reetsingh274@gmail.com>
2026-05-17 18:33:05 +0530

    Update help.py

--- ShashankMusic/plugins/bot/help.py
+++ ShashankMusic/plugins/bot/help.py
@@ -25,18 +25,18 @@ from config import BANNED_USERS, SUPPORT_CHAT
 from strings import get_string, helpers
 
 SHASHANK_PIC = [
-    "https://files.catbox.moe/qhxsg2.jpg",
-    "https://files.catbox.moe/3y5e6c.jpg",
-    "https://files.catbox.moe/9aif4j.jpg",
-    "https://files.catbox.moe/edkdm2.jpg",
-    "https://files.catbox.moe/qhxsg2.jpg",
-    "https://files.catbox.moe/9aif4j.jpg",
-    "https://files.catbox.moe/5wfpxe.jpg",
-    "https://files.catbox.moe/3y5e6c.jpg",
-    "https://files.catbox.moe/edkdm2.jpg",
-    "https://files.catbox.moe/5wfpxe.jpg",
-    "https://files.catbox.moe/edkdm2.jpg",
-    "https://files.catbox.moe/edkdm2.jpg"
+    "https://n.uguu.se/VZFsTBYe.jpg",
+    "https://d.uguu.se/FiKJAuTl.jpg",
+    "https://d.uguu.se/WVTDlkOJ.jpg",
+    "https://d.uguu.se/dSswAqSk.jpg",
+    "https://n.uguu.se/VZFsTBYe.jpg",
+    "https://d.uguu.se/FiKJAuTl.jpg",
+    "https://d.uguu.se/WVTDlkOJ.jpg",
+    "https://d.uguu.se/WVTDlkOJ.jpg",
+    "https://d.uguu.se/dSswAqSk.jpg",
+    "https://n.uguu.se/VZFsTBYe.jpg",
+    "https://d.uguu.se/FiKJAuTl.jpg",
+    "https://d.uguu.se/WVTDlkOJ.jpg"
 ]
 
 @app.on_message(filters.command(["help"]) & filters.private & ~BANNED_USERS)
